9ad0a1dcd767d12bcf5d34cf3af45288fd1697f2
Mettaton2654 <save2654@mail.ru>
2026-02-25 11:01:30 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import time
-from flask import Flask, render_template, redirect, url_for, flash, request
+from flask import Flask, render_template, redirect, url_for, flash, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
