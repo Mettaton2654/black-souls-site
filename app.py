63a60a378a4510c36c4d85f6203ca80c95d2d027
Mettaton2654 <save2654@mail.ry>
2026-04-13 08:06:59 +1000

    _and

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import socket
 from flask import Flask, render_template, redirect, url_for, flash, request, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
-from sqlalchemy import text
+from sqlalchemy import text, or_, and_
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
